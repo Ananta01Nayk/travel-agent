7db4c32c81c0e06e4433e006f18ae22136644891
Ananta Nayak <anantanayak337@gmail.com>
2026-07-12 20:32:55 +0530

    change frontend url to variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://travel-agent-5yk7.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
